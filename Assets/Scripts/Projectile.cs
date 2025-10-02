3a0dc123ecf9e4a94346492ed0c022fe9fdd473e
Canvas <peter.martingell@tripledotstudios.com>
2025-10-02 15:35:19 +0100

    Fix sea mesh and size, tweak pinball color, add trail+PS to projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Pinball
@@ -7,12 +8,29 @@ namespace Pinball
     {
         [SerializeField]
         private Rigidbody rigidBody;
+        [SerializeField]
+        private ParticleSystem onCollideParticles;
+
+        // we get a collision when the projectile is spawned,
+        // so ignore the first for now
+        private bool ignoreFirstCollision = true;
 
         public Rigidbody Rigidbody => rigidBody;
 
         private void OnValidate()
         {
             rigidBody = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (ignoreFirstCollision)
+            {
+                ignoreFirstCollision = false;
+                return;
+            }
+            
+            onCollideParticles.Play();
         }
     }
 }
